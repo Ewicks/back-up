7468780da4b705db905a521d71b022789d87b29b
ethan <ethanowicks9@gmail.com>
2024-02-08 16:55:37 +0000

    a

--- bot/settings.py
+++ bot/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
