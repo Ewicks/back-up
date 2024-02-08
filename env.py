ca98ade6e671f9372d9fb050a0d210b511440865
ethan <ethanowicks9@gmail.com>
2024-02-08 14:44:00 +0000

    Add

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 os.environ.setdefault("API-KEY", "LCX9HG6HVFNSR6YAACVCVSKJS1S7HHRD1Y99HC8YEJJA0JJSBJGBC8NA2UEZHH1S7PFN0CFNY0T2LCDO")
-os.environ.setdefault("DATABASE_URL", "postgres://bdstpcmg:ICndEvVIqUNouK7gj1SddU4_tnbscxXo@surus.db.elephantsql.com/bdstpcmg")
+os.environ.setdefault("DATABASE_URL", "postgres://tiqjxvvo:rlfXX1nJbHYheJ_xqa5GdYgx9YAHTLyf@surus.db.elephantsql.com/tiqjxvvo")
