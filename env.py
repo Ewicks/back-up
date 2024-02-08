2feaf3e2a2ecfd051aaf865c1c745e6c58e35f4c
ethan <ethanowicks9@gmail.com>
2024-02-08 16:48:23 +0000

    a

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 os.environ.setdefault("API-KEY", "LCX9HG6HVFNSR6YAACVCVSKJS1S7HHRD1Y99HC8YEJJA0JJSBJGBC8NA2UEZHH1S7PFN0CFNY0T2LCDO")
 os.environ.setdefault("DATABASE_URL", "postgres://tiqjxvvo:rlfXX1nJbHYheJ_xqa5GdYgx9YAHTLyf@surus.db.elephantsql.com/tiqjxvvo")
+os.environ.setdefault("SECRET_KEY", "^d_he7d5dorn6#$npdeqkxf0)1db*g(#223*rq8ylkr+kv6#rf")
+
